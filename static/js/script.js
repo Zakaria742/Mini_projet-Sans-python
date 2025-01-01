9835c8592d5aaeb4b8108022cc71f33b61a92559
Zakaria_Zaki <zaakizakaria12@gmail.com>
2025-01-01 20:17:41 +0100

    Added loading screen

--- static/js/script.js
+++ static/js/script.js
@@ -4,6 +4,10 @@ let nav = document.getElementById("nav-bar");
 function show_tab(){
     let navclass = nav.classList.toggle("show");
 }
-const root = document.documentElement;
-const currentTransform = getComputedStyle(root).getPropertyValue('--Htransform');
-root.style.setProperty('--Htransform', -560);
+window.addEventListener('load', function (){
+    const loadingScreen = document.getElementById('loading-screen');
+    const content = document.getElementById('cont');
+
+    loadingScreen.style.display = 'none';
+    content.style.display = 'block';
+})
